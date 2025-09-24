6c0b6a3f34674b79cef52eff4b15225fa3697e84
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2025-09-23 20:12:07 -0500

    feat: add a procedure to update a list of local envs which are using an editable install of this package

--- .datalad/procedures/update-local-envs.sh
+++ .datalad/procedures/update-local-envs.sh
@@ -2,10 +2,12 @@
 
 # Directories
 
+export VIRTUAL_ENV=
+
 list_of_env=(
-    "~/work/facsimilab/universe/tools"
-    "~/work/facsimilab/rapids-transcriptomics"
-    "~/work/prrx1/scRNA-seq"
+    "/home/pranav/work/facsimilab/universe/tools"
+    "/home/pranav/work/facsimilab/rapids-transcriptomics"
+    "/home/pranav/work/prrx1/scRNA-seq"
 )
 
 # Update each environment
@@ -13,6 +15,7 @@ for env_dir in "${list_of_env[@]}"; do
     echo "Updating environment in $env_dir"
     cd "$env_dir" || { echo "Directory $env_dir not found! Skipping..."; continue; }
 
+    echo $(pwd)
     uv sync
 
 done
